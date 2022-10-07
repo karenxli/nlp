051bc46e2ae99475ce85350eca9b3efefb839fbe
Karen Li <li.kar@northeastern.edu>
2022-10-06 22:31:54 -0400

    yeah fuck this

--- lm.py
+++ lm.py
@@ -14,12 +14,6 @@ CS 4120 NLP HW #2
 
 # Feel free to implement helper functions
 
-def splitText(training_file_path):
-    f = open(training_file_path, "r")
-    return f.read().split()
-
-    close(training_file_path)
-
 
 class LanguageModel:
   # constants to define pseudo-word tokens
@@ -34,22 +28,40 @@ class LanguageModel:
       n_gram (int): the n-gram order of the language model to create
       is_laplace_smoothing (bool): whether or not to use Laplace smoothing
     """
-    self.gramCount= n_gram
-    self.smooth = is_laplace_smoothing
-    self.unigram_frequencies = collections.Counter()
+    self.gramCount= n_gram                              # type of n_gram
+    self.smooth = is_laplace_smoothing                  # laplace smoothing?
+    self.unigram_frequencies = collections.Counter()    # unigram frequencies
     self.corpus_length = 0
-    self.corpus = []
 
+  # populates the frequency list per article
   def count(self, article):
     for word in article:
         self.unigram_frequencies[word] = self.unigram_frequencies.get(word, 0) + 1
 
+  # tokenizes a given txt file
   def splitText(self, training_file_path):
     f = open(training_file_path, "r")
     text = f.read().split()
     f.close()
     return text
 
+
+ # calculates the probabilities of all elements in a single sentence
+ # returns a list of that sentence's probabilities
+  def probability(self, sentence):
+    count = 0
+    for m in self.unigram_frequencies:
+        count = count + self.unigram_frequencies[m]
+    #count is now the total of all values - make this a general constant?
+
+    prob = []
+    for word in sentence:
+        if(self.unigram_frequencies[word] == 0 
+            & self.unigram_frequencies[self.UNK] > 0):
+                prob.append(self.unigram_frequencies[self.UNK]/count)
+        else: 
+            prob.append(self.unigram_frequencies[word]/count)
+    return prob
 
   def train(self, training_file_path):
     """Trains the language model on the given data. Assumes that the given data
@@ -61,19 +73,24 @@ class LanguageModel:
     Returns:
     None
     """
-    self.corpus = self.splitText(training_file_path)
-    self.count(self.corpus)
+    self.corpus = self.splitText(training_file_path) #tokenizes
+    self.count(self.corpus) # does an initial count of all words
+    
     unknowns = []
-    for num in self.unigram_frequencies:
+    for num in self.unigram_frequencies: # finds all the single words 
         if self.unigram_frequencies[num] == 1:
             unknowns.append(num)
+    
     for word in unknowns:
         word = " " + word + " "
         self.corpus = [t.replace(word, self.UNK) for t in self.corpus]
         del self.unigram_frequencies[word]
-    self.count(self.corpus)
-    #print(self.corpus)
-    #print(self.unigram_frequencies)
+    #self.count(self.corpus)
+
+    # it's not doing the replacement correctly
+    print(unknowns)
+    print(self.corpus)
+    print(self.unigram_frequencies)
 
 
   def score(self, sentence):
@@ -84,8 +101,16 @@ class LanguageModel:
     Returns:
       float: the probability value of the given string for this model
     """
-    pass
+    sent_prob = 1
 
+    sent_text = sentence.split()
+    total_prob = self.probability(sent_text)
+ 
+    for x in total_prob:
+        sent_prob *= x
+    return sent_prob
+
+    # this score can be very very small
   def generate_sentence(self):
     """Generates a single sentence from a trained language model using the Shannon technique.
       
--- test_minitrainingprovided.py
+++ test_minitrainingprovided.py
@@ -19,20 +19,24 @@ class TestMiniTraining(unittest.TestCase):
     bigram = LanguageModel(2, True)
     self.assertEqual(1, 1, msg="tests constructor for 2, True")
 
-  def test_unigramlaplace(self):
-    lm = LanguageModel(1, True)
-    lm.train("training_files/iamsam.txt")
   
+  def test_unigram(self):
+    lm = LanguageModel(1, False)
+    lm.train("training_files/iamsam2.txt")
+    # ((4) / (20))
+    self.assertAlmostEqual(.2, lm.score("<s>"), msg="tests probability of <s>, trained on iamsam2.txt")
+    # (2 / 20)
+    self.assertAlmostEqual(.1, lm.score("sam"), msg="tests probability of sam, trained on iamsam2.txt")
+    # (4 / 20) * (2 / 20) * (4 / 20)
+    self.assertAlmostEqual(.004, lm.score("<s> ham </s>"), msg="tests probability of <s> ham </s>, trained on iamsam2.txt")
+
   def test_unigramunknowns(self):
     lm = LanguageModel(1, False)
     lm.train("training_files/unknowns_mixed.txt")
+    # ((1) / (11))
+    self.assertAlmostEqual(1 / 11, lm.score("flamingo"), places=3, msg="tests probability of flamingo, trained on unknowns_mixed.txt")
 
-  def test_onlyunknownsgenerationandscoring(self):
-    lm = LanguageModel(1, True)
-    lm.train("training_files/unknowns.txt")
+  
 
-  def test_berptraining(self):
-    lm = LanguageModel(1, True)
-    lm.train("training_files/berp-training.txt")
 if __name__ == "__main__":
   unittest.main()
